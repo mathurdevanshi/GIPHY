bc46f88382641ce4bc4402b140fb8b76f4bb4887
Devanshi Mathur <devanshimathur@Yuvrajs-MacBook-Pro.local>
2019-03-09 16:44:22 -0800

    create new buttons, get new gifs to appear

--- assets/javascript.js
+++ assets/javascript.js
@@ -4,11 +4,13 @@ var gifCount=0;
 var gifItem;
 var gifButton;
 var gifID;
+var person;
+var queryURL;
 // ///////////////////////////////////////ARRAYS///////////////////////////////////////
 // ///////////////////////////////////////FUNCTIONS////////////////////////////////////
-// //-----------------------------------------------------------------------------------  gif button creater
-$("#gif-button").on("click", function() {
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=ajx51k7HYzwYxx7EkWBKRME3UIbFiJrx&limit=10";
+// //----------------------------------------------------------------------------------------------  gif creator
+function createGifs(buttonName){
+    queryURL = "http://api.giphy.com/v1/gifs/search?q="+buttonName+"&api_key=ajx51k7HYzwYxx7EkWBKRME3UIbFiJrx&limit=10";
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -21,31 +23,30 @@ $("#gif-button").on("click", function() {
         gifImage.attr("alt", "gif image");
         $("#images").prepend(gifImage);
     });
-});
-
+};
+// //----------------------------------------------------------------------------------------------  Handles Default Buttons
+$("button").on("click", function(){
+    createGifs(this.id);
+    });
+// //----------------------------------------------------------------------------------------------  Button Creator
 $("#add-gifButton").on("click", function(event2){
-    event2.preventDefault();
-        console.log("we clicked da new catagory button");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=ajx51k7HYzwYxx7EkWBKRME3UIbFiJrx&limit=10";
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-      })
-      .then(function(){
-          gifItem=$("#gifphy").val();
-              console.log("gifItem",gifItem);
-          gifButton =$("<button>");
-              console.log("gifButton",gifButton);
-          gifCount++;
-              console.log("gifCount",gifCount);
-          gifID="item-"+ gifCount;
-              console.log("gifID",gifID);
-          gifButton.attr("id", gifID);
-          gifButton.text(gifItem);
-          $("#buttons").append(gifButton);
-          $("#gifphy").val("");
-      })
+  event2.preventDefault();
+        gifItem=$("#gifphy").val();
+            console.log("gifItem",gifItem);
+        gifButton =$("<button>");
+            console.log("gifButton",gifButton);
+        gifCount++;
+            console.log("gifCount",gifCount);
+        gifID=gifItem;
+            console.log("gifID",gifID);
+        gifButton.attr("id", gifID);
+        gifButton.text(gifItem);
+        gifButton.on('click',function(){
+            console.log("FOCUS HERE", this.id);
+            createGifs(this.id);
+        });
+        $("#buttons").append(gifButton);
+            console.log("New Button Added",$(gifButton).val());
+        $("#gifphy").val("");
 });
-// //-----------------------------------------------------------------------------------  just some routine testings
-
-      
+// $("#gif-button").click(createGifs(this.id), console.log("CLICK!"));
